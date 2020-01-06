9b6507499a69d53df1652232fdd9fef67631c49c
tbauer42 <trist57890@gmail.com>
2020-01-06 15:17:58 +0100

    r

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="feuille-de-styles.css">
+    <title>Le titre du document</title>
+    <meta name="description" content="description de la page" />
+  </head>
+
+  <body>
+
+  <?php include 'include/_header.php' ?>
+
+  <?php include 'include/_main_index.php' ?>
+
+  <?php include 'include/_footer.php' ?>
+  
+  </body>
+
+</html>
